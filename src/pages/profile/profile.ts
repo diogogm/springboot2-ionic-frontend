477109e5c5c4917a6f49d09ab93d1a980bedf9da
Diogo Monteiro <diogo.g.monteiro@gmail.com>
2019-05-11 16:36:33 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,7 +25,11 @@ export class ProfilePage {
     public camera: Camera) {
   }
 
-  ionViewDidLoad() {
+  ionViewDidLoad(){
+    this.loadData();
+  } 
+  
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -68,5 +72,19 @@ export class ProfilePage {
       this.cameraOn = false;
      }, (err) => {
      });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+      });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
